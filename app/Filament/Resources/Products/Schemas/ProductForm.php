fe0165a4991c7d9bf9f8baf01a1abe699442b8b6
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2025-09-18 12:38:46 -0600

    cruds de productos y usuario en filamnetphp

---
+++ app/Filament/Resources/Products/Schemas/ProductForm.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Filament\Resources\Products\Schemas;
+
+use Filament\Forms\Components\Hidden;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+
+class ProductForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                Hidden::make('user_id')
+                    ->default(auth()->id())
+                    ->required(),
+                TextInput::make('name')
+                    ->required(),
+                Textarea::make('description')
+                    ->rows(3)
+                    ->required(),
+                TextInput::make('quantity')
+                    ->required()
+                    ->numeric()
+                    ->default(1),
+                TextInput::make('price')
+                    ->required()
+                    ->numeric()
+                    ->default(1)
+                    ->prefix('$'),
+            ]);
+    }
+}
